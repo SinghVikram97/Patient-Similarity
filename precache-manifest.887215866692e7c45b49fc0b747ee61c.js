85226d8f28a3ecee844c85ceac4b6e6eb50b3e08
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-10 19:19:34 +0530

    Updates

---
+++ precache-manifest.887215866692e7c45b49fc0b747ee61c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "333907e175a39d22a173",
+    "url": "/Patient-Similarity/static/js/runtime~main.333907e1.js"
+  },
+  {
+    "revision": "ea9b082d50b42b4fa121",
+    "url": "/Patient-Similarity/static/js/main.ea9b082d.chunk.js"
+  },
+  {
+    "revision": "f2b76636275ac8bfc5a7",
+    "url": "/Patient-Similarity/static/js/1.f2b76636.chunk.js"
+  },
+  {
+    "revision": "ea9b082d50b42b4fa121",
+    "url": "/Patient-Similarity/static/css/main.57c8b728.chunk.css"
+  },
+  {
+    "revision": "f2b76636275ac8bfc5a7",
+    "url": "/Patient-Similarity/static/css/1.a2f0aca5.chunk.css"
+  },
+  {
+    "revision": "edf0e433a13b68a0be925f3208a61170",
+    "url": "/Patient-Similarity/index.html"
+  }
+];
